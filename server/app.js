aed552c2f7877c4d167334214b9938b7f0da3da5
Trương Tiến Minh <truongtienminh0501@gmail.com>
2026-08-04 12:58:17 +0700

    Prepare backend for Railway

--- server/app.js
+++ server/app.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello Express");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 app.listen(PORT, () => {
